b3e5b5ea4e74e7b6b2f8a7970ad98125c03a3277
resideonocteam <alic.antunez@resideo.com>
2026-03-09 20:26:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // Replace 'resideo-noc-wan' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/resideo-noc-wan/",
+  base: "/noc-wan-report/",
 });
